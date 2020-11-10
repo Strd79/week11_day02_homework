d46495c48b610724537059199619ecc4ac07e3b3
Strd79 <strd79@hotmail.co.uk>
2020-11-10 21:18:45 +0000

    First commit

---
+++ src/main/java/Library.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Library {
+
+    private ArrayList<HashMap> collectionOfBooks;
+    private HashMap<String, String> book;
+
+    public Library() {
+        this.collectionOfBooks = new ArrayList<HashMap>();
+        this.book = new HashMap<String, String>();
+    }
+
+
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Library library;
+
+    @Before
+    public void before() {
+        library = new Library();
+    }
+
+    @Test
+    public void startsWithNoBooks() {
+        assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void canAddBook() {
+        library.addBook();
+    }
+}
